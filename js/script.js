f34e19a7036fe5fa5e3afeb5198010cf7f77ec84
Dominika Orlinska <dominika.orlinska1@gmail.com>
2024-03-01 13:47:33 +0100

    added functions to js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".js-themeName");
 
-button.addEventListener("click", () => {
+{
+  const toggleBackground = () => {
+  const themeName = document.querySelector(".js-themeName");
+  const body = document.querySelector(".js-body");
   body.classList.toggle("body--dark");
-
   themeName.innerText = body.classList.contains("body--dark")
     ? "jasny"
     : "ciemny";
-});
+};
+
+const init = () => {
+ const button = document.querySelector(".js-button");
+ button.addEventListener("click", toggleBackground);
+};
+ 
+
+init();
+
+}
 
