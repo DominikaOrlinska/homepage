1d7deacd2a2e3a0e8a6656c040e64dea531ba322
Dominika Orlinska <dominika.orlinska1@gmail.com>
2024-03-02 22:05:11 +0100

    code formatted

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
-
 {
   const toggleBackground = () => {
-  const themeName = document.querySelector(".js-themeName");
-  const body = document.querySelector(".js-body");
-  body.classList.toggle("body--dark");
-  themeName.innerText = body.classList.contains("body--dark")
-    ? "jasny"
-    : "ciemny";
-};
+    const themeName = document.querySelector(".js-themeName");
+    const body = document.querySelector(".js-body");
+    body.classList.toggle("body--dark");
+    themeName.innerText = body.classList.contains("body--dark")
+      ? "jasny"
+      : "ciemny";
+  };
 
-const init = () => {
- const button = document.querySelector(".js-button");
- button.addEventListener("click", toggleBackground);
-};
- 
+  const init = () => {
+    const button = document.querySelector(".js-button");
+    button.addEventListener("click", toggleBackground);
+  };
 
-init();
-
+  init();
 }
-
